237022f5543d5ae389138cffab4f313fa91e0aa6
Bryce Bradshaw <brycewbradshaw@icloud.com>
2020-05-22 09:37:10 -0600

    StoryBoard Completed

---
+++ Pair/Controllers/View Controller/PairListTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PairListTableViewController.swift
+//  Pair
+//
+//  Created by Bryce Bradshaw on 5/22/20.
+//  Copyright © 2020 Bryce Bradshaw. All rights reserved.
+//
+
+import UIKit
+
+class PairListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+ 
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+
+}
---
+++ Pair/Views/PairTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PairTableViewCell.swift
+//  Pair
+//
+//  Created by Bryce Bradshaw on 5/22/20.
+//  Copyright © 2020 Bryce Bradshaw. All rights reserved.
+//
+
+import UIKit
+
+class PairTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
